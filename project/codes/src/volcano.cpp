0066c99f090ce7f20b8abc0fcaa990c7dbda57b6
DXSheep <32773264+DXSheep@users.noreply.github.com>
2019-11-17 19:03:12 +0100

    Add files via upload

---
+++ project/codes/src/volcano.cpp
@@ -0,0 +1,76 @@
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkActor.h>
+#include <vtkPolyData.h>
+#include <vtkCoordinate.h>
+#include <vtkSphereSource.h>
+#include <vtkButtonWidget.h>
+#include <vtkTexturedButtonRepresentation2D.h>
+
+#include <vtkSmartPointer.h>
+#include <vtkMetaImageReader.h>
+#include <vtkMarchingCubes.h>
+#include <vtkImageData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPolyDataReader.h>
+#include <vtkSliderWidget.h>
+#include <vtkSliderRepresentation.h>
+#include <vtkSliderRepresentation2D.h>
+#include <vtkProperty2D.h>
+#include <vtkTextProperty.h>
+#include <vtkOpenGLGPUVolumeRayCastMapper.h>
+#include <vtkVolumeProperty.h>
+#include <vtkCamera.h>
+
+void readVTKFile()
+{
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
+	renWin->AddRenderer(renderer);
+
+
+	vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	iren->SetRenderWindow(renWin);
+
+
+	vtkSmartPointer<vtkPolyDataReader> vtkReader = vtkSmartPointer<vtkPolyDataReader>::New();
+	vtkReader->SetFileName("D:\\documents\\visualization\\project\\Volcanoes\\airs\\volcano_2011_150_am.vtk");
+
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(vtkReader->GetOutputPort());
+	mapper->ScalarVisibilityOff();
+
+
+	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+
+	vtkSmartPointer<vtkCamera> aCamera = vtkSmartPointer<vtkCamera>::New();
+	aCamera->SetViewUp(0, 0, -1);
+	aCamera->SetPosition(0, 1, 0);
+	aCamera->SetFocalPoint(0, 0, 0);
+	aCamera->ComputeViewPlaneNormal();
+	aCamera->Azimuth(30.0);
+	aCamera->Elevation(30.0);
+	aCamera->Dolly(1.5);
+
+
+	renderer->AddActor(actor);
+	renderer->SetActiveCamera(aCamera);
+	renderer->ResetCamera();
+	renderer->SetBackground(.2, .3, .4);
+	renderer->ResetCameraClippingRange();
+
+
+	renWin->Render();
+	iren->Initialize();
+	iren->Start();
+}
+
+int main()
+{
+	readVTKFile();
+	return 0;
+}
