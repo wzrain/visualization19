78b7badb74378f75492d416e4d484c97c0096a61
Marshalleeeeee <marshallee413lmc@sina.com>
2019-11-21 13:46:31 +0100

    volcanoArgv: get filename from argv

---
+++ project/codes/src/CMakeFiles/volcanoArgv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/volcanoArgv.dir/volcanoArgv.cpp.o"
+  "volcanoArgv.pdb"
+  "volcanoArgv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/volcanoArgv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
